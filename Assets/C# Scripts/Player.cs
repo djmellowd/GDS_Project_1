d4a9bb0796b9f1064fbca1c6da8592ce5756f431
Pawel Milczanowski <pawel.milczanowski@outlook.com>
2020-12-23 15:38:53 +0100

    Resolution Full HD & Player Horizontal Move

---
+++ Assets/C# Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 250.0f;
+
+    private Rigidbody2D _body;
+
+    void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        Vector2 movement = new Vector2(deltaX, _body.velocity.y);
+        _body.velocity = movement;
+    }
+}
