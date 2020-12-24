4012b74b68a2509895156e33e29405152f261da1
Pawel Milczanowski <pawel.milczanowski@outlook.com>
2020-12-24 14:53:10 +0100

    Enemy Flying

--- Assets/C# Scripts/FollowCam.cs
+++ Assets/C# Scripts/FollowCam.cs
@@ -4,9 +4,13 @@ using System.Collections;
 public class FollowCam : MonoBehaviour
 {
     public Transform target;
+    public float smoothTime = 0.2f;
+
+    private Vector3 _velocity = Vector3.zero;
 
     void LateUpdate()
     {
-        transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z);
+        Vector3 targetPosition = new Vector3(target.position.x, transform.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, smoothTime);
     }
 }
