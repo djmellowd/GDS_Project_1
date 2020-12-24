af2c6d0a450e0770b0a97071c01646c6e889978a
Pawel Milczanowski <pawel.milczanowski@outlook.com>
2020-12-24 15:39:03 +0100

    Basic AI

--- Assets/C# Scripts/BasicFlyingAI.cs
+++ Assets/C# Scripts/BasicFlyingAI.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BasicFlyingAI : MonoBehaviour
+public class BasicFlyingShootingAI : MonoBehaviour
 {
+    [SerializeField] private GameObject fireballPrefab;
+    private GameObject _fireball;
+
     public float speed = 3.0f;
     public float obstacleRange = 5.0f;
 
@@ -14,12 +17,21 @@ public class BasicFlyingAI : MonoBehaviour
         RaycastHit hit;
         if (Physics.SphereCast(ray, 0.75f, out hit))
         {
-            if (hit.distance < obstacleRange)
+            GameObject hitObject = hit.transform.gameObject;
+            if (hitObject.GetComponent<Player>())
+            {
+                if (_fireball == null)
+                {
+                    _fireball = Instantiate(fireballPrefab) as GameObject;
+                    _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                    _fireball.transform.rotation = transform.rotation;
+                }
+            }
+            else if (hit.distance < obstacleRange)
             {
                 float angle = Random.Range(-110, 110);
                 transform.Rotate(0, angle, 0);
             }
         }
-
     }
 }
---
+++ Assets/C# Scripts/BasicFlyingShootingAI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicFlyingAI : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float obstacleRange = 5.0f;
+
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.SphereCast(ray, 0.75f, out hit))
+        {
+            if (hit.distance < obstacleRange)
+            {
+                float angle = Random.Range(-110, 110);
+                transform.Rotate(0, angle, 0);
+            }
+        }
+
+    }
+}
