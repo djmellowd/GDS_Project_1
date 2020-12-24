150b14a850db0b4916df9f2ff2703e77b9500e6a
Pawel Milczanowski <pawel.milczanowski@outlook.com>
2020-12-24 14:18:51 +0100

    Camera Following

---
+++ Assets/C# Scripts/FollowCam.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour
+{
+    public Transform target;
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z);
+    }
+}
