cff310634ebfa14598152de5a2b91a6666cfa8cb
Pawel Milczanowski <pawel.milczanowski@outlook.com>
2020-12-27 03:58:49 +0100

    Title Screen

---
+++ Assets/C# Scripts/TitleScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}
