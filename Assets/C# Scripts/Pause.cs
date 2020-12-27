08e27c7e35cbeb0fa80efbc87768bdcd8e51f1b7
Bartosz Fedorowicz <fedorowicz93@gmail.com>
2020-12-27 14:36:36 +0100

    Ektan Pauzy (tymczasowy)

--- Assets/C# Scripts/Pause.cs
+++ Assets/C# Scripts/Pause.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class Pause : MonoBehaviour
 {
+
+    //odwołanie do ekranu pauzy
+    public GameObject pausePanel;
+
     void Start()
     {
         Time.timeScale = 1;
@@ -14,11 +18,18 @@ public class Pause : MonoBehaviour
         {
             if (Time.timeScale == 1)
             {
+                //wyświetlenie ekranu pauzy
+                pausePanel.SetActive(true);
+
                 Time.timeScale = 0;
+                
             }
             else if (Time.timeScale == 0)
             {
                 Time.timeScale = 1;
+
+                //ukrycie ekranu pauzy
+                pausePanel.SetActive(false);
             }
         }
     }
