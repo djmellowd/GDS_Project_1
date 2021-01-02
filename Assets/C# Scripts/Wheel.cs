a08f1420a1f27eb503b7b632e4b542c1a3a08173
Pawel Milczanowski <pawel.milczanowski@outlook.com>
2021-01-02 16:24:03 +0100

    No bouncing - problem with double jump

--- Assets/C# Scripts/Wheel.cs
+++ Assets/C# Scripts/Wheel.cs
@@ -3,22 +3,8 @@ using System.Collections;
 
 public class Wheel : MonoBehaviour
 {
-    PlayerCollider m_playerCollider;
-    Player _player;
-
-    void Start()
-    {
-        m_playerCollider = GetComponent<PlayerCollider>();
-        _player = FindObjectOfType<Player>();
-    }
-
     void Update()
     {
         transform.Rotate(0, 0, -1);
-
-        if (m_playerCollider.IsOnGround == false)
-        {
-            transform.position = new Vector2(_player.transform.position.x, _player.transform.position.y - 0.5f);
-        }
     }
 }
