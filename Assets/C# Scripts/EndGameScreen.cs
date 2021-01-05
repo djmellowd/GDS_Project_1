0af3b899140413e8f348f05563a21f2aaa8f057a
Bartosz Fedorowicz <fedorowicz93@gmail.com>
2021-01-05 13:31:39 +0100

    score shows on end screens

--- Assets/C# Scripts/EndGameScreen.cs
+++ Assets/C# Scripts/EndGameScreen.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class EndGameScreen : MonoBehaviour
 {
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI highscoreText;
+
+    private void Start()
+    {
+        scoreText.text = "Your Score: " + Player.score.ToString();
+        highscoreText.text = "Highscore: " + Player.highscore.ToString();
+    }
+
     void Update()
     {
         //wciśnięcie escape powoduje powrót do ekranu startowego
