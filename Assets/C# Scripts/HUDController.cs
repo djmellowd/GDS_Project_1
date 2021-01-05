84983516808c08db64e8f306332c2778ae1016ec
Bartosz Fedorowicz <fedorowicz93@gmail.com>
2021-01-05 15:28:38 +0100

    HUD

--- Assets/C# Scripts/HUDController.cs
+++ Assets/C# Scripts/HUDController.cs
@@ -2,21 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class HUDController : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI highscoreText;
+    public TextMeshProUGUI timeText;
+    public TextMeshProUGUI livesText;
+    public Image progressBar;
+    public Image live1;
+    public Image live2;
+    public Image live3;
+
+    [SerializeField] Transform startGamePoint;
+    Meta endGamePoint;
+    Player player;
+
+    float gameLength;
+    float playerDistance;
+    float distanceDifference;
+
+    float timer = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = FindObjectOfType<Player>();
+        endGamePoint = FindObjectOfType<Meta>();
+
+        gameLength = Mathf.Abs(endGamePoint.transform.position.x - startGamePoint.position.x);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //counting time
+        timer += Time.deltaTime;
+
+        timeText.text = "TIME " + ((int)timer).ToString();
         scoreText.text = Player.score.ToString();
         highscoreText.text = Player.highscore.ToString();
+        ManageLives();
+        CountDistance();
+    }
+
+    void CountDistance()
+    {
+        playerDistance = Mathf.Abs(endGamePoint.transform.position.x - player.transform.position.x);
+        distanceDifference = gameLength - playerDistance;
+
+        progressBar.rectTransform.localScale = new Vector3(distanceDifference / gameLength,1,1);
+    }
+
+    void ManageLives()
+    {
+        livesText.text = player.lives.ToString();
+
+        if (player.lives == 2)
+        {
+            live3.enabled = false;
+        }
+        else if (player.lives == 1)
+        {
+            live2.enabled = false;
+        }
+        else if(player.lives <= 0)
+        {
+            live1.enabled = false;
+        }
     }
 }
