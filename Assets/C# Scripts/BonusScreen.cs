c354c73454f154f735c3fc59cc5ebe8638cbdd3d
Bartosz Fedorowicz <fedorowicz93@gmail.com>
2021-01-11 21:13:00 +0100

    Update BonusScreen.cs

--- Assets/C# Scripts/BonusScreen.cs
+++ Assets/C# Scripts/BonusScreen.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BonusScreen : MonoBehaviour
 {
+    public TextMeshProUGUI playerTimeText;
+    public TextMeshProUGUI averageTimeText;
+    public TextMeshProUGUI recordTimeText;
+    public TextMeshProUGUI bonusText;
+
     // Start is called before the first frame update
     void Start()
     {
