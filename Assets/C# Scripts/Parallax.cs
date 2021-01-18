fdaeae1b23190b531b56646b8b74c324a4352425
Bartosz Fedorowicz <fedorowicz93@gmail.com>
2021-01-18 21:08:47 +0100

    Parallax - ready for testing

--- Assets/C# Scripts/Parallax.cs
+++ Assets/C# Scripts/Parallax.cs
@@ -5,19 +5,22 @@ using UnityEngine.UIElements;
 
 public class Parallax : MonoBehaviour
 {
-    private float _length;
-    private float _startPosition;
-
     public GameObject cam;
     public float parallaxEffect;
 
+
     public bool resetAtCheckpoint = false;
+
+    private float _length;
+    private float _startPosition;
+    float _resetPositionMultiplier;
 
     // Start is called before the first frame update
     void Start()
     {
         _startPosition = transform.position.x;
         _length = GetComponent<SpriteRenderer>().bounds.size.x;
+        _resetPositionMultiplier = 1.5f;
     }
 
     // Update is called once per frame
@@ -31,8 +34,9 @@ public class Parallax : MonoBehaviour
 
         if (resetAtCheckpoint)
         {
-            _startPosition = _length*2;
+            _startPosition = (cam.transform.position.x-dist)*_resetPositionMultiplier;
             resetAtCheckpoint = false;
+            _resetPositionMultiplier -= 0.25f;
         }
     }
 }
