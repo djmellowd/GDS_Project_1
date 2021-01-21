22a803f06921128c90ad8f8711f3ae782ba6bb8c
Bartosz Fedorowicz <fedorowicz93@gmail.com>
2021-01-21 17:57:39 +0100

    Upper HUD

---
+++ Assets/C# Scripts/CautionScanner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CautionScanner : MonoBehaviour
+{
+    public GameObject enemyCautionSprite;
+    public GameObject rockCautionSprite;
+    public GameObject mineCautionSprite;
+    public TextMeshProUGUI cautionText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 11) // enemy
+        {
+            enemyCautionSprite.SetActive(true);
+            cautionText.text = "Enemy Attack";
+        }
+        if (collision.gameObject.layer == 15) // obstacle
+        {
+            rockCautionSprite.SetActive(true);
+            cautionText.text = "Rocks Ahead";
+        }
+        if (collision.gameObject.layer == 16) //mines
+        {
+            mineCautionSprite.SetActive(true);
+            cautionText.text = "Minefield Ahead";
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 11) // enemy
+        {
+            enemyCautionSprite.SetActive(false);
+            cautionText.text = " ";
+        }
+        if (collision.gameObject.layer == 15) // obstacle
+        {
+            rockCautionSprite.SetActive(false);
+            cautionText.text = " ";
+        }
+        if (collision.gameObject.layer == 16) //mines
+        {
+            mineCautionSprite.SetActive(false);
+            cautionText.text = " ";
+        }
+    }
+}
