b3de4035df5f43a1a491e8fc90c04558d4a3af33
Bartosz Fedorowicz <fedorowicz93@gmail.com>
2021-01-23 16:41:38 +0100

    hole reset - in progress

---
+++ Assets/C# Scripts/GroundController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundController : MonoBehaviour
+{
+    public GameObject[] tiles;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
