4db937fec9c1686c8cba8d4ad443b375a37b85e7
Bartosz Fedorowicz <fedorowicz93@gmail.com>
2021-01-26 13:25:53 +0100

    added death on collision with elite

--- Assets/C# Scripts/Player.cs
+++ Assets/C# Scripts/Player.cs
@@ -250,7 +250,7 @@ public class Player : MonoBehaviour
     //zderzenie z wrogiem
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == 11)
+        if (collision.gameObject.layer == 11 || collision.gameObject.layer == 17)
         {
             Destroy(collision.gameObject);
             LooseLife();
